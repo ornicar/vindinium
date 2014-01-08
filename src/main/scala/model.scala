2624bb6401dcb007a10f7db64b70c657dd4c0a44
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-01-08 09:56:21 +0100

    regroup small models

---
+++ src/main/scala/model.scala
@@ -0,0 +1,31 @@
+package jousse.org
+package bot
+
+case class Pos(x: Int, y: Int) {
+
+  def left = copy(x = x - 1)
+  def right = copy(x = x + 1)
+  def up = copy(y = y - 1)
+  def down = copy(y = y + 1)
+
+  def neighbors = Set(left, right, up, down)
+}
+
+sealed trait Dir
+case object Dir {
+  case object Up
+  case object Down
+  case object Left
+  case object Right
+}
+
+sealed abstract class Tile(char: Char) {
+  override def toString = char.toString
+}
+object Tile {
+  case object Air extends Tile(' ')
+  case object Wall extends Tile('#')
+  case object Potion extends Tile('!')
+  case object Monster extends Tile('x')
+  case class Hero(number: Int) extends Tile(number.toString.head)
+}
