e7fa47972d93fc67e3f62e685899d9db4989a2ae
Vincent Velociter <vincent.velociter@gmail.com>
2014-01-11 01:27:09 +0100

    Improve nav, add turn number

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,38 @@
 $(function() {
     $(window).load(function() {
+
         var currentPos = 0;
-        drawPosition(replay.games[currentPos]);
+
+        function updateGame(pos) {
+            drawPosition(replay.games[pos]);
+            $('#turn > span').text(Math.floor(pos / 4));
+        }
+
+        // game
+        updateGame(currentPos);
+
+        // nav
         $('#nav').show();
         $('#prev').click(function(e) {
             if (currentPos > 0) {
                 currentPos--;
-                drawPosition(replay.games[currentPos]);
+                updateGame(currentPos);
             }
         });
         $('#next').click(function(e) {
             if (currentPos < replay.games.length - 1) {
                 currentPos++;
-                drawPosition(replay.games[currentPos])
+                updateGame(currentPos);
             }
         });
-
+        $('#board').mousewheel(function(event) {
+            if (event.deltaY == -1) {
+                $('#next').click();
+            } else if (event.deltaY == 1) {
+                $('#prev').click();
+            }
+            event.stopPropagation();
+            return false;
+        });
     });
 });
