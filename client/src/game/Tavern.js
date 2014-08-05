7091aeee1b4a13004fcdd68b8e32cbc704d95702
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-08-05 21:51:59 +0200

    polish Tavern position and fix sprite order on x

--- client/src/game/Tavern.js
+++ client/src/game/Tavern.js
@@ -4,6 +4,7 @@ var beerTexture = PIXI.Texture.fromImage("/assets/img/beer2.png");
 
 function Tavern () {
   PIXI.Sprite.call(this, beerTexture);
+  this.position.x = 4;
   this.position.y = -8;
 }
 Tavern.prototype = Object.create(PIXI.Sprite.prototype);
